1006fbe3f2ca5c63a5ceb9d4f3da3553955d4440
Anna Kristensen <anna.k.kristensen@hotmail.com>
2022-02-09 15:08:13 +0100

    Init

---
+++ Pages/Om.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ommig.Pages
+{
+    public class OmModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿
+//ändrar färg på h1 rubrik efter tid och ger olika hälsningar beroende på tid.
+const hour = new Date().getHours(); 
+let greeting;
+
+if (hour < 11) {
+    greeting = "God morgon!";
+    var heading = document.getElementsByTagName("h1")[0];
+    heading.style.color = "rgb(21, 85, 49)";
+} else if (hour < 17) {
+    greeting = "Ha en travlig dag!";
+    var heading = document.getElementsByTagName("h1")[0];
+    heading.style.color = "rgb(153, 60, 50)";
+} else  {
+    greeting = "Ha en trevlig kväll!";
+    var heading = document.getElementsByTagName("h1")[0];
+    heading.style.color = "black";
+}
+
+document.getElementById("js").innerHTML = "<h3>" + greeting + "</h3>";
+
